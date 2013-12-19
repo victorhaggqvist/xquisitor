98aaf80f630bf05dac4403b8f416b29285662914
Victor Häggqvist <victor@snilius.com>
2013-12-19 03:19:41 +0100

    start scripts

--- XQuisitor2.bat
+++ XQuisitor2.bat
@@ -1,1 +1,1 @@
-java -Xbootclasspath/p:lib/rsyntaxtextarea.jar:lib/saxon9he.jar:lib/AppleJavaExtensions.jar -jar out/artifacts/naked/naked.jar 
+java -Xbootclasspath/p:lib/rsyntaxtextarea.jar:lib/saxon9he.jar:lib/AppleJavaExtensions.jar -jar out/artifacts/XQuisitor2/XQuisitor2.jar
